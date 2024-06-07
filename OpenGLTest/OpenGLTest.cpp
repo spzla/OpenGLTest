2999aa3278cbcda76105e61f6407db3ec4d04747
spzla <57840318+spzla@users.noreply.github.com>
2024-06-07 21:52:24 +0200

    coordinate transformations

--- OpenGLTest/OpenGLTest.cpp
+++ OpenGLTest/OpenGLTest.cpp
@@ -131,15 +131,28 @@ int main()
     glActiveTexture(GL_TEXTURE1);
     glBindTexture(GL_TEXTURE_2D, texture2);
 
-    glm::mat4 trans = glm::mat4(1.0f);
-    trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
+    shader.use();
+
+    //glm::mat4 trans = glm::mat4(1.0f);
+    //trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
     //trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
     //trans = glm::rotate(trans, (float)-glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
 
-    shader.setMat4("transform", trans);
+    glm::mat4 model = glm::mat4(1.0f);
+    model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+
+    glm::mat4 view = glm::mat4(1.0f);
+    // note that we're translating the scene in the reverse direction of where we want to move
+    view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+
+    glm::mat4 projection;
+    projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+
+    shader.setMat4("model", model);
+    shader.setMat4("view", view);
+    shader.setMat4("projection", projection);
     shader.setFloat("mixValue", mixValue);
 
-    shader.use();
     glBindVertexArray(VAO);
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
